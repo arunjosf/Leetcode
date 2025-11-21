e617010d11f28a19930fd9dac78432a9c937c568
Arun Joseph <arunjoseph400@gmail.com>
2025-11-21 12:31:28 +0530

    Time: 0 ms (100%), Space: 54.3 MB (52.84%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+     let index = nums.indexOf(target);
+  if (index !== -1) return index; 
+   for (let i = 0; i < nums.length; i++) {
+    if (target < nums[i]) return i;
+  }
+  return nums.length;
+};
