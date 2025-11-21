d175e2a140ffa8079480d8604c9bd79857a8992e
Arun Joseph <arunjoseph400@gmail.com>
2025-11-21 12:47:46 +0530

    Time: 0 ms (100%), Space: 53.1 MB (96.03%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,6 +3,14 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    digits[digits.length -1] = digits[digits.length -1] +1;
+   for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+
+    digits.unshift(1);
     return digits;
 };
