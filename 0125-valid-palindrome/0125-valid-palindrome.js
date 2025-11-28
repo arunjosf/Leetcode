45768477d631c79c01b0f4d807079594530a22d5
Arun Joseph <arunjoseph400@gmail.com>
2025-11-28 16:44:06 +0530

    Time: 4 ms (76.42%), Space: 58.2 MB (53.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, '');
-    let reversed = cleaned.split('').reverse().join('');
-    return cleaned === reversed;
+    let alpha = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reversed = alpha.split('').reverse().join('');
+    return alpha === reversed;
 };
